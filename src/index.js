e1bd713ee1ce3e2b1d681d9634ea9c0c911bb2ea
Kyeongheui Sohng <kheeya7@hotmail.com>
2018-07-20 08:27:19 -0700

    Initial commit with the boiler plate codes 0720

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './style.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app'
+
+ReactDOM.render(<
+  App title="Kate Sohng | React project 2018" />,
+  document.getElementById('app-react-root')
+)
