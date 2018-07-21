17011a3cc269ebaaccff2013e5f38f15fc0580d2
Kyeongheui Sohng <kheeya7@hotmail.com>
2018-07-20 20:35:39 -0700

    created Header component with the navbar and carousal

---
+++ src/react-portfolio/header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Nav, Navbar, NavItem } from 'react-bootstrap'
+import ControlledCarousel from './carousel.js'
+
+export default class HeaderView extends React.Component {
+    render() {
+        return (
+            <div>
+                <Navbar inverse>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="#home">KS</a>
+                        </Navbar.Brand>
+                    </Navbar.Header>
+                    <Nav pullRight>
+                        <NavItem eventKey={1} href="#">
+                            About
+                        </NavItem>
+                    </Nav>
+                </Navbar>
+                <ControlledCarousel />
+            </div>
+        )
+    }
+}
---
+++ src/react-portfolio/page-contents.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class PageContentsView extends React.Component {
+    render() {
+        return (
+            <div>
+                Portfolio Contents
+            </div>
+        )
+    }
+}
