730a2542e954c8510b0ad1263ac44d6c75eb7994
Kyeongheui Sohng <kheeya7@hotmail.com>
2018-07-25 21:18:12 -0700

    Added a mordal for the filter component

--- src/components/filter-control.js
+++ src/components/filter-control.js
@@ -1,10 +1,62 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 
 export default class FilterControl extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.onModalContainerClicked = this.onModalContainerClicked.bind(this)
+    }
+
+    onModalContainerClicked(e) {
+        if (e.target.id === 'modal-container') {
+            this.props.closeFilter()
+        }
+    }
+
     render() {
+        const displayStyle = this.props.isFilterOpen ? "block" : "none"
+
+        const modalStyle = {
+            display: displayStyle,
+            position: "fixed",
+            zIndex: 10,
+            left: 0,
+            top: 0,
+            width: "100%",
+            height: "100%",
+            overflow: "auto",
+            backgroundColor: "rgba(0,0,0,0.4)",
+        }
+
+        const modalContentStyle = {
+            backgroundColor: "#fefefe",
+            margin: "15% auto",
+            padding: "20px",
+            border: "1px solid #888",
+            width: "50%",
+        }
+
         return (
             <div>
-                Filter Control
+                <Button onClick={this.props.openFilter}>
+                    Filter Control
+                </Button>
+                <div
+                    id="modal-container"
+                    style={modalStyle}
+                    onClick={this.onModalContainerClicked}
+                >
+                    <div style={modalContentStyle}>
+                        <span
+                            className="close"
+                            onClick={this.props.closeFilter}
+                        >
+                            &times;
+                        </span>
+                        <p> Some text in the modal...</p>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/main/index.js
+++ src/main/index.js
@@ -4,11 +4,37 @@ import ProjectList from '../components/project-list'
 import FilterControl from '../components/filter-control'
 
 export default class MainView extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isFilterOpen: false,
+        }
+        this.openFilter = this.openFilter.bind(this)
+        this.closeFilter = this.closeFilter.bind(this)
+    }
+
+    openFilter() {
+        this.setState({
+            isFilterOpen: true,
+        })
+    }
+
+    closeFilter() {
+        this.setState({
+            isFilterOpen: false,
+        })
+    }
+
     render() {
         return (
             <div>
                 <Carousel />
-                <FilterControl />
+                <FilterControl
+                    isFilterOpen={this.state.isFilterOpen}
+                    openFilter={this.openFilter}
+                    closeFilter={this.closeFilter}
+                />
                 <ProjectList selectProjectById={this.props.selectProjectById} />
             </div>
         )
