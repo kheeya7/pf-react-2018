7c8dc95b112cdac050aa82d9c2a4e39db54ec7b6
Kyeongheui Sohng <kheeya7@hotmail.com>
2018-08-27 15:53:01 -0700

    Modified footer

--- src/footer/index.js
+++ src/footer/index.js
@@ -1,52 +1,63 @@
 import React from 'react'
 import { Grid, Row, Col } from 'react-bootstrap'
-
-const footerContainerStyle = {
-
-    backgroundColor: "rgba(0,0,0,0.9)",
-    color: "#fff",
-}
-
-const footerRowStyle = {
-    margin: "20px 0 0 0",
-}
-
-const footerCopyrightStyle = {
-    margin: "20px 0 0 0",
-    paddingTop: "20px",
-    textAligh: "center",
-    borderTop: '1px solid #fff'
-}
-
-const footerColTitleStyle = {
-    fontSize: '1.2em',
-    fontWeight: '800'
-}
-
-const footerColBodyStyle = {
-    fintWeight: 'normal',
-}
+import './index.less'
 
 export default class FooterView extends React.Component {
     render() {
         return (
-            <div style={footerContainerStyle}>
+            <div className='footer-container'>
                 <Grid>
-                    <Row style={footerRowStyle}>
-                        <Col xs={6} md={4} style={footerColTitleStyle}>
-                            Links from Kate
-                            <Row>
-                                magazine? what happened?
-                            </Row>
+                    <Row className='footer-row'>
+                        <Col xs={6} md={4}>
+                            <div className='title'>
+                                Links from Kate
+                            </div>
+                            <div>
+                                <ul>
+                                    <li>
+                                        <a href='/'>Home</a>
+                                    </li>
+                                    <li>
+                                        <a href='/about'>About Me</a>
+                                    </li>
+                                    <li>
+                                        <a href='http://katesohng.com/blog' target='_blank'>Blog </a>
+                                    </li>
+                                    <li>
+                                        <a href='http://katesohng.com/' target='_blank'>Portfolio 2013</a>
+                                    </li>
+                                </ul>
+                            </div>
                         </Col>
-                        <Col xs={6} md={4} style={footerColTitleStyle}>
-                            Place holder text abcd
+                        <Col xs={6} md={4}>
+                            <div className='title'>
+                                Latest Updates
+                            </div>
+                            <div>
+                            </div>
                         </Col>
-                        <Col xs={6} md={4} style={footerColTitleStyle}>
-                            Follow me on
+                        <Col xs={6} md={4}>
+                            <div className='title'>
+                                Follow Me on
+                            </div>
+                            <div>
+                                <ul>
+                                    <li>
+                                        <a href='https://github.com/kheeya7' className='btn btn-social btn-github btn-simple' target='_blank'>
+                                            <i className="fa fa-github"></i> GitHub</a>
+                                    </li>
+                                    <li>
+                                        <a href='https://www.linkedin.com/in/kate-sohng/' className='btn btn-social btn-linkedin btn-simple' target='_blank'>
+                                            <i className="fa fa-linkedin"></i> LinkedIn</a>
+                                    </li>
+                                    <li>
+                                        <a href='http://katesohng.com/blog' className='btn btn-social btn-linkedin btn-simple' target='_blank'><i className="fa fa-instagram"></i> Instagram </a>
+                                    </li>
+                                </ul>
+                            </div>
                         </Col>
                     </Row>
-                    <Row style={footerCopyrightStyle}>
+                    <Row className='copyright'>
                         @2018 Kate Sohng
                     </Row>
                 </Grid>
--- webpack.config.js
+++ webpack.config.js
@@ -12,29 +12,41 @@ module.exports = {
     },
     module: {
         rules: [{
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            }
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        },
+        {
+            test: /\.less$/,
+            use: [
+                {
+                    loader: 'style-loader'
+                }, {
+                    loader: 'css-loader'
+                }, {
+                    loader: 'less-loader'
+                }
+            ]
+        },
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+                'file-loader'
+            ]
+        },
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: [
+                'file-loader'
+            ]
+        },
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+        }
         ]
     },
     resolve: {
