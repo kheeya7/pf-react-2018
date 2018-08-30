1d510accd506bed080e028718fc4e75ea9540a8c
Kyeongheui Sohng <kheeya7@hotmail.com>
2018-08-29 18:41:25 -0700

    initiated a work experience component

---
+++ src/components/work-experience.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+
+export default class WorkExperience extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
